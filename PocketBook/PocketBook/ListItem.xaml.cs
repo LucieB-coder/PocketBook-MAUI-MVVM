8ee9099c6690eff5af233a5018ba65d288494efa
lucie.bedouret <lucie.bedouret@etu.uca.fr>
2023-09-06 17:37:54 +0200

    ✨ LibraryPage

---
+++ PocketBook/PocketBook/ListItem.xaml.cs
@@ -0,0 +1,31 @@
+using AndroidX.CardView.Widget;
+
+namespace PocketBook;
+
+public partial class ListItem : ContentView
+{
+    public static readonly BindableProperty ImageSourceProperty = BindableProperty.Create(nameof(ImageSource), typeof(string), typeof(ListItem), string.Empty);
+    public static readonly BindableProperty ItemNameProperty = BindableProperty.Create(nameof(ItemName), typeof(string), typeof(ListItem), string.Empty);
+    public static readonly BindableProperty NumberOfElementsProperty = BindableProperty.Create(nameof(NumberOfElements), typeof(string), typeof(ListItem), string.Empty);
+
+    public string ImageSource
+    {
+        get => (string)GetValue(ListItem.ImageSourceProperty);
+        set => SetValue(ListItem.ImageSourceProperty, value);
+    }
+    public string ItemName
+    {
+        get => (string)GetValue(ListItem.ItemNameProperty);
+        set => SetValue(ListItem.ItemNameProperty, value);
+    }
+
+    public string NumberOfElements
+    {
+        get => (string)GetValue(ListItem.NumberOfElementsProperty);
+        set => SetValue(ListItem.NumberOfElementsProperty, value);
+    }
+    public ListItem()
+	{
+		InitializeComponent();
+    }
+}
