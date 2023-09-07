e28c84c85746b3b4a3b717c41dd7bddcf9c18030
lucie.bedouret <lucie.bedouret@etu.uca.fr>
2023-09-07 20:00:06 +0200

    ✨ LibraryPage finished

--- PocketBook/PocketBook/MauiProgram.cs
+++ PocketBook/PocketBook/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
 
 namespace PocketBook
 {
@@ -9,6 +10,8 @@ namespace PocketBook
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                // Initialize the .NET MAUI Community Toolkit by adding the below line of code
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
