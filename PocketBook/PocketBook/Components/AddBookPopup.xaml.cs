9557886b9fedb237b0c73f8195514757b7b99609
lucie.bedouret <lucie.bedouret@etu.uca.fr>
2023-09-24 11:14:14 +0200

    ✨ Modal on "Saisir l'ISBN"

--- PocketBook/PocketBook/Components/AddBookPopup.xaml.cs
+++ PocketBook/PocketBook/Components/AddBookPopup.xaml.cs
@@ -1,10 +1,14 @@
+using PocketBook.ViewModel;
+
 namespace PocketBook.Components;
 
 public partial class AddBookPopup
 {
-	public AddBookPopup()
+    public NavigationViewModel NavigationViewModel { get; set; } = new NavigationViewModel();
+
+    public AddBookPopup()
 	{
 		InitializeComponent();
+		BindingContext = this;
 	}
-
 }
--- PocketBook/PocketBook/ViewModel/NavigationViewModel.cs
+++ PocketBook/PocketBook/ViewModel/NavigationViewModel.cs
@@ -13,15 +13,23 @@ namespace PocketBook.ViewModel
         public event PropertyChangedEventHandler PropertyChanged;
 
         public ICommand NavigateCommand { get; set; }
+        public ICommand DisplayModalCommand { get; set; }
 
         public NavigationViewModel() 
         {
             NavigateCommand = new Command<string>(GoToRoute);
+            DisplayModalCommand = new Command<string>(DisplayModal);
         }
 
         private async void GoToRoute(string route)
         {
             await Shell.Current.GoToAsync(route);
+        }
+
+        private async void DisplayModal(string message) 
+        {
+            /* Is this Okay ?? */
+            await Application.Current.MainPage.DisplayAlert("Info", message, "OK");
         }
     }
 }
